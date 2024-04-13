c2ff212651ec1c70e9bc909484290982e3778b5a
justcode <justicejahnadindu@gmail.com>
2024-04-13 04:53:15 -1100

    initial commit

--- chatpage/ChatApp/chat.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.cion').addEventListener('mouseover', function(event) {
-  if (event.target.id === 'btn') {
-     document.getElementById('popup').classList.remove('hidden');
-  }
- });
- 
- document.querySelector('.cion').addEventListener('mouseout', function() {
-  document.getElementById('popup').classList.add('hidden');
- });
--- chatpage/Chatbox/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.container1').addEventListener('mouseover', function(event) {
-  if (event.target.id === 'btn1') {
-     document.getElementById('popup').classList.remove('hidden');
-  }
- });
- 
- document.querySelector('.container1').addEventListener('mouseout', function() {
-  document.getElementById('popup').classList.add('hidden');
- });
